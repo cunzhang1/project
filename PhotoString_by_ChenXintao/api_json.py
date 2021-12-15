278551153d7e3b3930bc2b4d52f1296e8c62c5c1
cunzhang1 <96170644+cunzhang1@users.noreply.github.com>
2021-12-15 20:57:16 +0800

    Add files via upload

--- PhotoString_by_ChenXintao/api_json.py
+++ PhotoString_by_ChenXintao/api_json.py
@@ -12,16 +12,19 @@ def api_json():  # 获取所有图片信息
     rq.instructions("SELECT * FROM photo ORDER By time desc")
     rq.do()
     lst = []  # 存储输出的图片信息的数组
-    page = ''
+    page = "图片信息：" + '<p>'
     i = 1  # 图片是的序号
     for r in rq.format_results().split('\n\n'):
         photo = r.split(',')
-        picture_time = photo[0]  # 获取图片的上传时间
-        picture_description = photo[1]  # 获取图片的描述
-        picture_path = photo[2].strip()  # 获取图片的存储路径
-        photo_size = str(format((os.path.getsize(picture_path) / 1024), '.2f')) + 'KB'  # 获取图片的文件大小，单位为KB
-        lst = [{'ID': i, 'upload_date': picture_time, 'description': picture_description, 'photo_size': photo_size}]  # 将图片信息存储到lst信息中
-        lst2 = json.dumps(lst[0], sort_keys=True, indent=4, separators=(',', ':'))  # 转换成json对象
-        page +="图片%d: " '%s' %(i,lst2) +'<p>'  # 在网页中输出图片信息
-        i += 1
+        if photo[0] != '':
+            picture_time = photo[0]  # 获取图片的上传时间
+            picture_description = photo[1]  # 获取图片的描述
+            picture_path = photo[2].strip()  # 获取图片的存储路径
+            photo_size = str(format((os.path.getsize(picture_path) / 1024), '.2f')) + 'KB'  # 获取图片的文件大小，单位为KB
+            lst = [{'ID': i, 'upload_date': picture_time, 'description': picture_description, 'photo_size': photo_size}]  # 将图片信息存储到lst信息中
+            lst2 = json.dumps(lst[0], sort_keys=True, indent=4, separators=(',', ':'))  # 转换成json对象
+            page +="图片%d: " '%s' %(i,lst2) +'<p>'  # 在网页中输出图片信息
+            i += 1
+        else:
+            page += "无图片"
     return page
